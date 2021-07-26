e2940acdd562f2050eb786f807effceb4168d1f6
sunyoring <sunhyo67@gmail.com>
2021-07-26 20:19:19 +0900

    2021-07-26

--- STUDYNOTE/study.js
+++ STUDYNOTE/study.js
@@ -1,10 +1,60 @@
 
 
+window.onload = function(){
+    for(let i=0; i<localStorage.length; i++) {
+        let key = localStorage.key(i);
+     
+            field.innerHTML += `${key} ==> ${localStorage.getItem(key)}` + "<br>";
+      }
+
+}
+
+var date = new Date();
+var time = String(date.getFullYear()+"/"+date.getMonth()+"/" +date.getDate()+" " +date.getHours()+":"+date.getMinutes());
+var text="";
+
 function start() {
-    var text = document.getElementById("text").value;
+
+    text = "시작!!!";
     var field = document.getElementById("field");
  
-        field.innerHTML += "* "+text + "<br>";
-        localStorage.setItem(text);
+        localStorage.setItem(time,text);
+
+
+        for(let i=0; i<localStorage.length; i++) {
+            let key = localStorage.key(i);
+            if(key == time){
+                field.innerHTML += `${key} ==> ${localStorage.getItem(key)}` + "<br>";
+
+            }
+          }
+}
+
+
+function end() {
+
+    text ="종료!!!"
+    
+    var field = document.getElementById("field");
+
+    localStorage.setItem(time,text);
+  
+    for(let i=0; i<localStorage.length; i++) {
+        let key = localStorage.key(i);
+        if(key == time){
+            field.innerHTML += `${key} ==> ${localStorage.getItem(key)}` + "<br>";
+
+        }
+      }
+}
+
+function reset(){
+
+    var field = document.getElementById("field");
+
+    localStorage.clear();
+    alert("모든 기록이 삭제되었습니다.");
+    field.innerHTML="";
+
 
 }
